976634243f2989da498132e099a322f5a77ed41b
IamThiago-IT <thiagodossantos315@gmail.com>
2024-08-21 17:38:34 +0000

    config geral

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { User, UserSchema } from './schemas/user.schema';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      secret: 'yourSecretKey',
+      signOptions: { expiresIn: '60m' },
+    }),
+    PassportModule,
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
